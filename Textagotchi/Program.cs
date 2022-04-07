a92899301816c5ce8b662b2306b8b153e7db602f
connor.hayward <connor.hayward@WLS-208-03>
2022-04-07 15:04:25 -0700

    Add project files.

---
+++ Textagotchi/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Welcome to a Text-Based Tamagotchi Clone ");
+Console.WriteLine("By Connor Hayward\n");
+
+Console.WriteLine("Enter your a name for your Tamagotchi:\n");
+
+
+    string Tama = Console.ReadLine();
+
+    Console.WriteLine(Tama +
