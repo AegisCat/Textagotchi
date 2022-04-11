0a0230ebf17c9896c25e6cffe5230d0b8385d298
Connor Haywrad <connor.hayward@sd27.bc.ca>
2022-04-11 14:25:08 -0700

    Added new prompts, overhauled while loop, added level variable

--- Textagotchi/Program.cs
+++ Textagotchi/Program.cs
@@ -10,8 +10,11 @@ namespace Textagotchi
         {
             string tama;
             string location;
+            int level = 1;
             int milliseconds = 5000;
             bool entry = false;
+            string[] help = {"help", "info"};
+            bool helpentry = false;
 
             Console.WriteLine("Enter your Pet's Name.");
             tama = Console.ReadLine();
@@ -27,21 +30,37 @@ namespace Textagotchi
 
             while (entry == true)
             {
-                Console.WriteLine("Enter a Prompt");
-                
-                string input = Console.ReadLine();
-
-                if (input == "info")
+                if (helpentry == false)
                 {
-                    Console.WriteLine("\nPet Name: " + tama);
-                    Console.WriteLine("\nLocation: " + location);
-
-                
+                    helpentry = true;
+                    Console.WriteLine("\nEnter 'help' to find commands.");
+                    Console.WriteLine("Enter a Prompt");
                 }
 
-
+                else
+                {
+                    Console.WriteLine("\nEnter a Prompt");
+                }
                 
+                string input = Console.ReadLine();
+                Console.WriteLine("");
 
+                if (input == "help")
+                {
+                    Array.ForEach(help, Console.WriteLine);
+
+                }
+                if (input == "info")
+                {
+                    Console.WriteLine("Pet Name: " + tama);
+                    Console.WriteLine("Location: " + location);
+                    Console.WriteLine("Level: " + level);
+                }
+
+                if (input == "among us")
+                {
+                    Console.WriteLine("You're Not Funny.");
+                }
             }
 
         }
