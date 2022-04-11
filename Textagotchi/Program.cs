9dcf3ba68d7baa09a31c07d7dd8983e53faf7b23
Connor Haywrad <connor.hayward@sd27.bc.ca>
2022-04-11 15:10:48 -0700

    Coingame Implemented. Leveling System Created.

--- Textagotchi/Program.cs
+++ Textagotchi/Program.cs
@@ -11,9 +11,10 @@ namespace Textagotchi
             string tama;
             string location;
             int level = 1;
+            float experience = 0;
             int milliseconds = 5000;
             bool entry = false;
-            string[] help = {"help", "info"};
+            string[] help = {"help", "info", "coingame"};
             bool helpentry = false;
 
             Console.WriteLine("Enter your Pet's Name.");
@@ -30,6 +31,63 @@ namespace Textagotchi
 
             while (entry == true)
             {
+                if (experience >= 10 && experience <= 29)
+                {
+                    level = 2;
+                    Console.WriteLine("\nLeveled Up!");
+
+                }
+
+                if (experience >= 30 && experience <= 59)
+                {
+                    level = 3;
+                    Console.WriteLine("\nLeveled Up!");
+
+                }
+
+                if (experience >= 60 && experience <= 99)
+                {
+                    level = 4;
+                    Console.WriteLine("\nLeveled Up!");
+
+                }
+
+                if (experience >= 100 && experience <= 199)
+                {
+                    level = 5;
+                    Console.WriteLine("\nLeveled Up!");
+
+                }
+
+                if (experience >= 200 && experience <= 499)
+                {
+                    level = 6;
+                    Console.WriteLine("\nLeveled Up!");
+
+                }
+
+                if (experience >= 500 && experience <= 999)
+                {
+                    level = 7;
+                    Console.WriteLine("\nLeveled Up!");
+
+                }
+
+                if (experience >= 1000 && experience <= 1999)
+                {
+                    level = 8;
+                    Console.WriteLine("\nLeveled Up!");
+
+                }
+
+                if (experience >= 2000)
+                {
+                    level = 9;
+                    Console.WriteLine("\nLeveled Up!");
+
+                }
+
+
                 if (helpentry == false)
                 {
                     helpentry = true;
@@ -55,11 +113,59 @@ namespace Textagotchi
                     Console.WriteLine("Pet Name: " + tama);
                     Console.WriteLine("Location: " + location);
                     Console.WriteLine("Level: " + level);
+                    Console.WriteLine("Experience: " + experience);
                 }
 
                 if (input == "among us")
                 {
                     Console.WriteLine("You're Not Funny.");
+                }
+
+                if (input == "coingame")
+
+                {
+                    int numPlays = 0, numWins = 0;
+                    int compChoice = 0;
+                    char userChoice;
+                    double percentWin;
+                    Random rnd = new Random();
+                    while (true)
+                    {
+                        Console.Write("\nWrite H/h to guess Heads, T/t to guess Tails, or Q/q to quit =>  ");
+                        userChoice = Console.ReadKey().KeyChar;
+                        compChoice = rnd.Next(0, 2);
+                        if (char.ToLowerInvariant(userChoice) != 'q')
+                        {
+                            if (char.ToLowerInvariant(userChoice) == 'h' && compChoice == 0)
+                            {
+                                Console.WriteLine("\nYOU WON");
+                                Console.WriteLine("\nGained 2 EXP");
+                                experience = experience + 2;
+                                numWins++;
+                            }
+                            else if (char.ToLowerInvariant(userChoice) == 't' && compChoice == 1)
+                            {
+                                Console.WriteLine("\nYOU WON");
+                                Console.WriteLine("\nGained 2 EXP");
+                                experience = experience + 2;
+                                numWins++;
+                            }
+                            else
+                            {
+                                Console.WriteLine("\nYOU LOSE");
+                            }
+                            numPlays++;
+                        }
+                        else
+                        {
+                            percentWin = (double)numWins / numPlays;
+                            Console.WriteLine("\nYou won {0} out of {1} game(s) or {2:P} of the games played.", numWins, numPlays, percentWin);
+                            break;
+                        }
+
+                    }
+
+
                 }
             }
 
