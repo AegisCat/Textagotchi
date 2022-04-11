5a7b96150c4e4a8b3c827249391ba7d00d72f1ac
Connor Haywrad <connor.hayward@sd27.bc.ca>
2022-04-11 12:03:10 -0700

    Entry Sequence Finished

--- Textagotchi/Program.cs
+++ Textagotchi/Program.cs
@@ -10,13 +10,17 @@ namespace Textagotchi
         {
             string textagotchi;
             string location;
+            int milliseconds = 5000;
+            Console.WriteLine("Enter your Pet's Name.");
             textagotchi = Console.ReadLine();
-            Console.WriteLine(textagotchi + ". What an interesting and special name that is totally unique. \n");
-            Console.WriteLine("Anyways, where does your Pikachu live?");
+            Console.WriteLine("What an interesting and special name that is totally unique. \n");
+            Console.WriteLine("Anyways, where does your " + textagotchi + " live?");
             location = Console.ReadLine();
+            Console.WriteLine("\nWell, who would have guessed. Anyways, Welcome and good luck taking care of your " + textagotchi);
+            Console.WriteLine("\nProceeding in 5 Seconds");
+            Thread.Sleep(milliseconds);
+            Console.Clear();
 
-
-            
         }
     }
 }
