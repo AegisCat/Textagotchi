d9ef8228f476a6671afe12e0a627b058e394cc2d
Connor Haywrad <connor.hayward@sd27.bc.ca>
2022-04-11 12:15:48 -0700

    First Prompt

--- Textagotchi/Program.cs
+++ Textagotchi/Program.cs
@@ -8,18 +8,41 @@ namespace Textagotchi
         public static void Main(string[] args)
 
         {
-            string textagotchi;
+            string tama;
             string location;
             int milliseconds = 5000;
+            bool entry = false;
+
             Console.WriteLine("Enter your Pet's Name.");
-            textagotchi = Console.ReadLine();
+            tama = Console.ReadLine();
             Console.WriteLine("What an interesting and special name that is totally unique. \n");
-            Console.WriteLine("Anyways, where does your " + textagotchi + " live?");
+            Console.WriteLine("Anyways, where does your " + tama + " live?");
             location = Console.ReadLine();
-            Console.WriteLine("\nWell, who would have guessed. Anyways, Welcome and good luck taking care of your " + textagotchi);
+            Console.WriteLine("\nWell, who would have guessed. Anyways, Welcome and good luck taking care of your " + tama);
+            
             Console.WriteLine("\nProceeding in 5 Seconds");
             Thread.Sleep(milliseconds);
             Console.Clear();
+            entry = true;
+
+            while (entry == true)
+            {
+                Console.WriteLine("Enter a Prompt");
+                
+                string input = Console.ReadLine();
+
+                if (input == "info")
+                {
+                    Console.WriteLine("\nPet Name: " + tama);
+                    Console.WriteLine("\nLocation: " + location);
+
+                
+                }
+
+
+                
+
+            }
 
         }
     }
