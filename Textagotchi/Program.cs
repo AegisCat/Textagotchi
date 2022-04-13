0680d6b6d9728d7e94e66e8d3afd604baafb368a
Connor Haywrad <connor.hayward@sd27.bc.ca>
2022-04-13 11:45:15 -0700

    Scrapped Leveling System for Currency System, Added ability to edit Tamagotchi and Location Names, default string values for Tamagotchi and Location set to "Tamagotchi" and "Your Pocket" Respectively.

--- Textagotchi/Program.cs
+++ Textagotchi/Program.cs
@@ -4,23 +4,29 @@
     {
 
         public static void Main(string[] args)
-
+            
         {
-            string tama;
-            string location;
-            int level = 1;
-            int experience = 0;
+            string? tama;
+            string? location;
+            int money = 0;
             int milliseconds = 5000;
             bool entry = false;
-            string[] help = {"help", "info", "coingame", "exptable"};
-            string[] exptable = { "Level 2 = 10", "Level 3 = 30", "Level 4 = 60", "Level 5 = 100", "Level 6 = 200", "Level 7 = 500", "Level 8 = 1000", "Level 9 = 2000" };
+            string[] help = {"help", "info", "coingame", "edit"};
             bool helpentry = false;
 
             Console.WriteLine("Enter your Pet's Name.");
             tama = Console.ReadLine();
+            if (tama == "")
+            {
+                tama = "Tamagotchi";
+            }
             Console.WriteLine("What an interesting and special name that is totally unique. \n");
             Console.WriteLine("Anyways, where does your " + tama + " live?");
             location = Console.ReadLine();
+            if (location == "")
+            {
+                location = "Your Pocket";
+            }
             Console.WriteLine("\nWell, who would have guessed. Anyways, Welcome and good luck taking care of your " + tama);
             
             Console.WriteLine("\nProceeding in 5 Seconds");
@@ -31,62 +37,6 @@
 
             while (entry == true)
             {
-
-                if (experience >= 10 && experience <= 29)
-                {
-                    level = 2;
-                    Console.WriteLine("\nLeveled Up!");
-
-                }
-
-                if (experience >= 30 && experience <= 59)
-                {
-                    level = 3;
-                    Console.WriteLine("\nLeveled Up!");
-
-                }
-
-                if (experience >= 60 && experience <= 99)
-                {
-                    level = 4;
-                    Console.WriteLine("\nLeveled Up!");
-
-                }
-
-                if (experience >= 100 && experience <= 199)
-                {
-                    level = 5;
-                    Console.WriteLine("\nLeveled Up!");
-
-                }
-
-                if (experience >= 200 && experience <= 499)
-                {
-                    level = 6;
-                    Console.WriteLine("\nLeveled Up!");
-
-                }
-
-                if (experience >= 500 && experience <= 999)
-                {
-                    level = 7;
-                    Console.WriteLine("\nLeveled Up!");
-
-                }
-
-                if (experience >= 1000 && experience <= 1999)
-                {
-                    level = 8;
-                    Console.WriteLine("\nLeveled Up!");
-
-                }
-
-                if (experience >= 2000)
-                {
-                    level = 9;
-                    Console.WriteLine("\nLeveled Up!");
-
-                }
 
                 if (helpentry == false)
                 {
@@ -100,11 +50,11 @@
                     Console.WriteLine("\nEnter a Prompt");
                 }
                 
-                string input = Console.ReadLine();
-                Console.WriteLine("");
+                string? input = Console.ReadLine();
 
-                if (input == "help")
+                if ((input == "help") || (input == "commands"))
                 {
+                    Console.WriteLine("");
                     Array.ForEach(help, Console.WriteLine);
 
                 }
@@ -112,12 +62,19 @@
                 {
                     Console.WriteLine("Pet Name: " + tama);
                     Console.WriteLine("Location: " + location);
-                    Console.WriteLine("Level: " + level);
-                    Console.WriteLine("Experience: " + experience);
+                    Console.WriteLine("Money Held: $" + money);
                     Console.WriteLine("Current Time: " + DateTime.Now.ToString());
                 }
 
-                if (input == "among us")
+                if (input == "edit")
+                {
+                    Console.WriteLine("Please enter a new name for your Tamagotchi");
+                    tama = Console.ReadLine();
+                    Console.WriteLine("Please enter a new location for your Tamagotchi");
+                    location = Console.ReadLine();
+                }
+
+                if ((input == "among us") || (input == "amongus") || (input == "sus"))
                 {
                     Console.WriteLine("You're Not Funny.");
                 }
@@ -129,7 +86,7 @@
                     int compChoice = 0;
                     char userChoice;
                     double percentWin;
-                    Random rnd = new Random();
+                    Random rnd = new();
                     while (true)
                     {
                         Console.Write("\nWrite H/h to guess Heads, T/t to guess Tails, or Q/q to quit =>  ");
@@ -140,15 +97,15 @@
                             if (char.ToLowerInvariant(userChoice) == 'h' && compChoice == 0)
                             {
                                 Console.WriteLine("\nYOU WON");
-                                Console.WriteLine("\nGained 2 EXP");
-                                experience = experience + 2;
+                                Console.WriteLine("\nYOU EARNED $2");
+                                money = money + 2;
                                 numWins++;
                             }
                             else if (char.ToLowerInvariant(userChoice) == 't' && compChoice == 1)
                             {
                                 Console.WriteLine("\nYOU WON");
-                                Console.WriteLine("\nGained 2 EXP");
-                                experience = experience + 2;
+                                Console.WriteLine("\nYOU EARNED $2");
+                                money = money + 2;
                                 numWins++;
                             }
                             else
@@ -161,17 +118,12 @@
                         {
                             percentWin = (double)numWins / numPlays;
                             Console.WriteLine("\nYou won {0} out of {1} game(s) or {2:P} of the games played.", numWins, numPlays, percentWin);
+                            Console.WriteLine("Won a Total of $" + money);
                             break;
                         }
 
                     }
 
-
-                }
-
-                if (input == "exptable")
-                {
-                    Array.ForEach(exptable, Console.WriteLine);
 
                 }
             }
